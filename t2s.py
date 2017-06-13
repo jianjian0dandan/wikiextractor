14f48c047623fe6f419246f6628f0ade8ad294c7
linhaobuaa <1257819385@qq.com>
2017-06-13 22:19:20 +0800

    update

--- t2s.py
+++ t2s.py
@@ -17,9 +17,31 @@ if __name__ == '__main__':
     folder = "./output/AA/"
     fnames = os.listdir(folder)
     keys = [u'url', u'text', u'id', u'title']
+
+    tempname = "tmpchttext"
+    tempoutputname = "tmpchstext"
+    fw = open(tempname, "w")
+    splitstr = "===================================================="
+    item_list = []
     for fname in fnames:
-    	if not fname.startswith("wiki"):
-    		continue
+        if not fname.startswith("wiki"):
+            continue
+        
+        f = open(os.path.join(folder, fname))
+        for line in f:
+            item = json.loads(line.strip())
+            item_list.append(item)
+        f.close()
+        """
+        f = open(os.path.join(folder, fname))
+        for line in f:
+            item = json.loads(line.strip())
+            item["text"] = cc.convert(item["text"])
+            fwt.write("%s\n" % json.dumps(item))
+        f.close()
+        """
+
+        """
         tempname = "tmpchttext"
         tempoutputname = "tmpchstext"
         f = open(os.path.join(folder, fname))
@@ -35,4 +57,29 @@ if __name__ == '__main__':
                 item["text"] = f1.read()
                 fwt.write("%s\n" % json.dumps(item))
         f.close()
+        """
+    for idx, item in enumerate(item_list):
+        if idx > 0:
+            fw.write(splitstr)
+        fw.write("%s" % item["text"].encode("utf-8"))
+        del item["text"]
+    fw.close()
+
+    print "start convert..."
+    cmd = "opencc-1.0.1-win64\opencc.exe -i %s -o %s -c opencc-1.0.1-win64\\t2s.json" % (tempname, tempoutputname)
+    os.popen(cmd)
+    print "end convert..."
+
+    texts_list = []
+    with open(tempoutputname) as f:
+        data = f.read()
+        texts_list = data.split(splitstr)
+    print "end split..."
+    print len(item_list), len(texts_list)
+    
+    for idx, text in enumerate(texts_list):
+        item = item_list[idx]
+        item["text"] = text.decode("utf-8")
+        fwt.write("%s\n" % json.dumps(item))
+
     fwt.close()
