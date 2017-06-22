4efc1fb5697f3299bafc02a01eff39fdcff7dac8
Hao Lin <linhao.lh@qq.com>
2017-06-22 17:47:11 +0800

    Add files via upload

---
+++ entity_extract/generate_self_dict.py
@@ -0,0 +1,26 @@
+#-*-coding: utf-8-*-
+
+import jieba
+import jieba.posseg as pseg
+from recognize_with_jieba import is_nt
+
+jieba.load_userdict("self_dict.txt")
+
+add_ins_titles = set()
+with open("ins_titles_zhwiki.txt") as f:
+    for line in f:
+        ins_title = line.strip()
+        result = is_nt(ins_title)
+        if result[1]:
+            add_ins_titles.add(ins_title)
+
+with open("self_dict.txt") as f:
+    for line in f:
+        data = line.strip().split(" ")
+        if data[2] == "nt":
+            add_ins_titles.add(data[0])
+
+fw = open("self_dict.txt", "w")
+for t in add_ins_titles:
+    fw.write("%s %s %s\n" % (t, "3", "nt"))
+fw.close()
