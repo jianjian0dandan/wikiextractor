5a68db37a6cd4e4ecc77df666f207de4ea5ab3c2
Hao Lin <linhao.lh@qq.com>
2017-06-28 17:23:53 +0800

    Add files via upload

--- entity_extract/recognize_with_nltk.py
+++ entity_extract/recognize_with_nltk.py
@@ -29,10 +29,10 @@ def extract_ne_en(text):
     for tree in chunked_sentences:
         entity_names.extend(extract_entity_names(tree))
 
-    ct = Counter(entity_names)
-    results = ct.most_common()
+    entity_names = list(set(entity_names))
+    result = map_entity2sent(text, entity_names)
 
-    return [r[0] for r in results]
+    return result
 
 
 def extract_entity_names(t):
@@ -47,27 +47,26 @@ def extract_entity_names(t):
 
     return entity_names
 
+def map_entity2sent(text, entities):
+    en_dict = dict()
+    sentences = nltk.sent_tokenize(text)
+    for sent in sentences:
+        for entity in entities:
+            if entity in sent:
+                try:
+                    en_dict[entity].append(sent)
+                except KeyError:
+                    en_dict[entity] = [sent]
+
+    return en_dict
+
+
 if __name__ == '__main__':
     texts = 'For other uses, see ISIL (disambiguation), ISIS (disambiguation), Daish (disambiguation), and Islamic state (disambiguation).Islamic State of Iraq and the Levantالدولة الإسلامية في العراق والشام‎ad-Dawlah al-Islāmiyah fī \'l-ʿIrāq wa-sh-ShāmParticipant in the Iraq War (2003–2011), Iraqi insurgency, Syrian Civil War, Iraqi Civil War, Second Libyan Civil War, Boko Haram insurgency, War in North-West Pakistan, War in Afghanistan, Yemeni Civil War, and other conflictsPrimary target of Operation Inherent Resolve and of the military intervention against ISIL: in Syria, Iraq, Libya, and Nigeria.ISIL originated as Jama\'at al-Tawhid wal-Jihad in 1999, which pledged allegiance to al-Qaeda and participated in the Iraqi insurgency following the 2003 invasion of Iraq by Western forces. The group proclaimed itself a worldwide caliphate[52][53] and began referring to itself as Islamic State (الدولة الإسلامية ad-Dawlah al-Islāmiyah) or IS[54] in June 2014. As a caliphate, it claims religious, political, and military authority over all Muslims worldwide.[55] Its adoption of the name Islamic State and its idea of a caliphate have been widely criticised, with the United Nations, various governments, and mainstream Muslim groups rejecting its statehood.[56]In Syria, the group conducted ground attacks on both government forces and opposition factions, and by December 2015 it held a large area in western Iraq and eastern Syria containing an estimated 2.8 to 8 million people,[57][58] where it enforced its interpretation of sharia law. ISIL is now believed to be operational in 18 countries across the world, including Afghanistan and Pakistan, with "aspiring branches" in Mali, Egypt, Somalia, Bangladesh, Indonesia, and the Philippines.[59][60][61][62] As of 2015, ISIL was estimated to have an annual budget of more than US$1 billion and a force of more than 30,000 fighters.[63]'
     print "-----------\n"
-    print extract_ne_en(texts)
-
-    with open("./20170612data/en/News/20535.en.txt") as f:
-        result = extract_ne_en(f.read())
-        print "-----------News en\n"
-        print result
-
-    with open("./20170612data/en/dissertation/CIA/686.en.txt") as f:
-        result = extract_ne_en(f.read())
-        print "-----------CIA en\n"
-        print result
-
-    with open("./20170612data/en/dissertation/Safety/1600.en.txt") as f:
-        result = extract_ne_en(f.read())
-        print "-----------Safety en\n"
-        print result
-
-    with open("./20170612data/en/dissertation/Armedorganization/974.en.txt") as f:
-        result = extract_ne_en(f.read())
-        print "-----------Armedorganization en\n"
-        print result
+    result = extract_ne_en(texts)
+    for k, v in result.iteritems():
+        print "实体名称: ", k
+        print "下面是实体出现的句子: "
+        for idx, sent in enumerate(v):
+            print "句子%s: " % idx, sent
